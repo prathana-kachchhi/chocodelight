a90e9184713630553ee61149b61bf71bee77865c
prathana-kachchhi <prathanarejoice@gmail.com>
2024-08-28 16:38:54 +0530

    wpadd

--- src/component/home/index.jsx
+++ src/component/home/index.jsx
@@ -4,13 +4,14 @@ import Section1 from './section1'
 
 import Section3 from './section3'
 import Section4 from './section4'
+import Section2 from './section2'
 
 export default function Home() {
   return (
     <div>
       <Herobanner />
       <Section1 />
-   
+   <Section2 />
       <Section3 />
       <Section4 />
     </div>
